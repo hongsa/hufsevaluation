c5efa430faae5cd71262d5d00d96039b47e7e671
System Administrator <root@JaneKim.local>
2014-12-12 17:01:08 +0900

    new

--- apps/controllers.py
+++ apps/controllers.py
@@ -1,19 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, redirect, url_for, render_template, request, flash, session, jsonify, make_response,current_app
 from apps import app,db
-<<<<<<< HEAD
-=======
-from apps.core.model.models import Actor, Filmo, Rating, Bookmark
-from core.model import models as models
-from core.controller import userController as userC
-from core.controller import videoController as videoC
-from core.controller import actorController as actorC
-from core.service import ratings as ratings
-from core.service import image as image
-from core.service import admin as adminS
-from core.service import etc as etc
->>>>>>> 942b695f89142e7c04f229f050afee774b83fe82
-
 
 @app.route('/')
 
