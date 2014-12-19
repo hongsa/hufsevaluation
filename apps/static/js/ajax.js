8efd9eb0e7fcf7f3f5e38a1a09c19b9e3800f78e
김재현 <nobjob@naver.com>
2014-12-19 18:17:15 +0900

    ss

--- apps/static/js/ajax.js
+++ apps/static/js/ajax.js
@@ -1,47 +1,37 @@
-// $(document).ready(function() {
-//    var number = 0;
-
-//    $.ajax({
-//       url:'/video_detail',
-//       dataType:'JSON',
-//       success:function(data){
-
-//          number = data.rows-5;
-
-//       }
-
-//    });
-
-//    function getArticle(id){
-
-//       $.ajax({
-//          url:'/more',
-//          dataType:'JSON',
-//          data:{
-//             number: id
-//          },
-//          success:function(data){
-//          	i=data.id
-//          	t=data.title
-//          	c=data.content
-//          	$("#article").append('<div class="panel panel-default"><div class="panel-heading"><a href="detail/' +i+ '">' +i+ ". " +t+ '</a></div><div class="panel-body">' +c+ '</div></div>');
-//          }
-//       });
-
-//    }
+/**
+ * Created by bebop on 2014-12-19.
+ */
 
 
-//    $('#load_more_button').click(function(){
-      
-//       if(number>=1){
-//       getArticle(number);
-//       number = number -1;
-//    }
-//       else{
-//          $('#load_more_button').hide();
-//       }
+$(document).ready(function(){
 
-      
-      
-//    });
-// });
+
+$('#mainModalButton').click(function () {
+
+    $.ajax({
+        url:'/login',
+        type:'POST',
+        dataType: 'JSON',
+        data:{
+
+        }
+
+
+
+
+
+    })
+
+
+
+
+
+
+})
+
+
+
+
+
+
+})
