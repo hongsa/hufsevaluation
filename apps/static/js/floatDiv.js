d232d28ec462234ef9845ae3edea424eb79ebd58
김재현 <nobjob@naver.com>
2014-12-21 17:15:54 +0900

    ss

--- apps/static/js/floatDiv.js
+++ apps/static/js/floatDiv.js
@@ -7,8 +7,8 @@ var stmnLEFT = 10; // 오른쪽 여백
 var stmnGAP1 = 0; // 위쪽 여백
 var stmnGAP2 = 150; // 스크롤시 브라우저 위쪽과 떨어지는 거리
 var stmnBASE = 150; // 스크롤 시작위치
-var stmnActivateSpeed = 35; //스크롤을 인식하는 딜레이 (숫자가 클수록 느리게 인식)
-var stmnScrollSpeed = 20; //스크롤 속도 (클수록 느림)
+var stmnActivateSpeed = 10; //스크롤을 인식하는 딜레이 (숫자가 클수록 느리게 인식)
+var stmnScrollSpeed = 10; //스크롤 속도 (클수록 느림)
 var stmnTimer;
 
 function RefreshStaticMenu() {
