5c387376a5581aaabef66ce90224d9e720bed156
hongg <sshongnim@naver.com>
2015-01-06 20:00:29 +0900

    댓글 엔터키 입력

--- apps/static/js/comment.js
+++ apps/static/js/comment.js
@@ -1,28 +1,35 @@
 /* 서버쪽으로 데이터를 보내보자 */
-var sendData = function() {
-    /* 일단 처리할 정보를 가져오자. ( jquery 이용 ) */
-    var inputComment = $('input[name=comment]').val();
-    var inputActorName = $('input[name=actorname]').val();
-    $('input[type="text"],textarea').val('');
-    var $target = $('html,body');
-    $target.animate({scrollTop: $target.height()}, 1000);
+$(document).ready(function(){
+    $('#actor_comment').click(function(){
+        /* 일단 처리할 정보를 가져오자. ( jquery 이용 ) */
+        var inputComment = $('input[name=comment]').val();
+        var inputActorName = $('input[name=actorname]').val();
+        $('input[type="text"],textarea').val('');
+        var $target = $('html,body');
+        $target.animate({scrollTop: $target.height()}, 1000);
 
-    /* 보낼 데이터 객체로 준비 ( 이게 제일 심플함. )
-     서버에서는 input 이라는 이름으로 데이터를 받기로 약속되어져있다. AjaxSample.py 참고 */
-    var sParam = {
-        comment : inputComment,
-        actorName : inputActorName
-    }
-    /* 보내보자 시바 */
-    $.ajax({
-        url : '/actor/comment', // 데이터 보낼 주소.
-        data : sParam, // 데이터 보낼것 ( key = value 쌍 구조를 이루고있어야됨 )
-        dataType: "JSON",
-        method : 'POST', // 포스트로 보내기로했지?
-        success: receiverHandler, // 핸들러 등록 아래 참고.
-        error: errorHandler //
+        /* 보낼 데이터 객체로 준비 ( 이게 제일 심플함. )
+         서버에서는 input 이라는 이름으로 데이터를 받기로 약속되어져있다. AjaxSample.py 참고 */
+        var sParam = {
+            comment: inputComment,
+            actorName: inputActorName
+        }
+        /* 보내보자 시바 */
+        $.ajax({
+            url: '/actor/comment', // 데이터 보낼 주소.
+            data: sParam, // 데이터 보낼것 ( key = value 쌍 구조를 이루고있어야됨 )
+            dataType: "JSON",
+            method: 'POST', // 포스트로 보내기로했지?
+            success: receiverHandler, // 핸들러 등록 아래 참고.
+            error: errorHandler //
+        });
     });
-}
+    $('#actor_input').keypress(function(e){
+       if(e.which == 13){
+           $('#actor_comment').click();
+       }
+    });
+});
 /* 서버에서 계산한 결과가 왔을때 처리할 부분 */
 var receiverHandler = function(result, textStatus, xhr) {
     $
