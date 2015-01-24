39f5348ffe330e035683183692cfab8c91f8cf5a
hongsa <hsasung@gmail.com>
2015-01-24 19:08:07 +0900

    신작영상이랑 기획사랑 나눔

---
+++ apps/controller/newVideo2.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from flask import redirect, url_for, render_template,flash, session
+from apps.models import Video,User
+from sqlalchemy import desc
+import math
+
+
+def new_video2(name, page):
+    # 로그인 안한 상태로 오면 index로 빠꾸
+    if not 'session_user_email' in session:
+        flash(u"로그인 되어있지 않습니다.", "error")
+        return redirect(url_for('index'))
+
+    releaseList = set([int(each.release) for each in Video.query.all()])
+    video = Video.query.filter(Video.release * 100 > name * 100,
+                                      Video.release * 100 < (name + 1) * 100)
+    videoRelease = video.order_by(desc(Video.release)).offset((page - 1) * 12).limit(12)
+    total = video.count()
+    calclulate = float(float(total) / 12)
+    total_page = math.ceil(calclulate)
+
+    if name == 0:
+        release = 0
+        videoRelease = Video.query.filter_by(release=0).offset((page - 1) * 12).limit(12)
+    else:
+        release = int(video.first().release)
+
+    email = session['session_user_email']
+    user = User.query.get(email)
+    rating = user.ratingsActor()
+
+    list = []
+    for v in videoRelease:
+        list.append(v.name)
+
+    ratingList=[]
+    for r in rating:
+        if r['name'] in list:
+            ratingList.append(dict(name = r['name'], rating=r['rating']))
+
+
+
+    a = float(math.ceil(float(page)/10))
+    if a ==1:
+        down=1
+    else:
+        down = int((a-1) * 10)
+
+    if total_page > a*10:
+        total_page = a * 10
+        up = int(total_page+1)
+
+    else:
+        up = int(total_page)
+
+
+    return render_template("new_video_main2.html", releaseList=releaseList, videoRelease=videoRelease, release=release,
+            total_page=range(1+(10*(int(a)-1)), int(total_page+1)), up = up, down = down,ratingList=ratingList,page=page)
--- apps/controllers.py
+++ apps/controllers.py
@@ -3,7 +3,7 @@ from flask import redirect, url_for, flash, session,render_template
 from apps import app
 from apps.controller import video
 from models import User,Actor
-from controller import user,actor,newActor,newVideo,search,admin,collection,star,bookmark,detail
+from controller import user,actor,newActor,newVideo,newVideo2,search,admin,collection,star,bookmark,detail
 import recommendation
 
 
@@ -91,6 +91,12 @@ def new_actor(name, page):
 @app.route('/n_video/<path:name>/<int:page>', methods=['GET', 'POST'])
 def new_video(name, page):
     return newVideo.new_video(name,page)
+
+@app.route('/n_video2/<int:name>', defaults={'page': 1})
+@app.route('/n_video2/<int:name>/<int:page>', methods=['GET', 'POST'])
+def new_video2(name, page):
+    return newVideo2.new_video2(name,page)
+
 #신작평가(newActorController, newVideoController) 끝
 
 
@@ -255,79 +261,79 @@ def recommend2():
     return render_template('recomm.html', rList=rList,count=count)
     # return 'well done'
 
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+from apps import app
+from bs4 import BeautifulSoup
+import urllib2
+import re
+import logging
+from sqlalchemy import desc
+from apps import db
 #
-# from flask import Flask, render_template
-# from apps import app
-# from bs4 import BeautifulSoup
-# import urllib2
-# import re
-# import logging
-# from sqlalchemy import desc
-# from apps import db
-#
-# @app.route('/crawling/<int:page>',defaults={'page':1})
-# @app.route('/crawling/<int:page>',methods=['GET', 'POST'])
-# def crawling(page):
-#     # 배우랑 키 가져오기
-#     a = Actor.query.order_by(desc(Actor.average)).offset((page - 1) * 20).limit(20)
-#     actor = []
-#     for i in a:
-#         actor.append(i.name.replace(" ","") )
-#
-#     final=[]
-#     for each in actor:
-#
-#         url = "http://hentaku.tistory.com/entry/" + str(each)
-#         try:
-#             source = urllib2.urlopen( url ).read()
-#         except:
-#             continue
-#         soup = BeautifulSoup( source ,from_encoding="utf-8")
-#
-#         list=""
-#         for total in soup.find_all("div","avstar_info_b"):
-#             list=total.text
-#         # logging.error(list)
-#
-#         # final = []
-#         name = re.compile(u"[^ \u3131-\u3163\uac00-\ud7a3]+")
-#         height = re.compile("[1][4-8][0-9]")
-#
-#         result1 = name.sub("",list)
-#         result2 = height.findall(list)
-#         str2 = ''.join(result2)
-#
-#         final.append(dict(name=result1,value=str2))
-#
-#
-#         logging.error(final)
-#         # logging.error(result1)
-#         # logging.error(result2)
-#
-#
-#     for each in final:
-#         actor = Actor.query.get(each['name'])
-#         if actor == None:
-#             continue
-#
-#         if each['value'] =="" :
-#             actor.height = 155
-#             each['value'] = 155
-#         else:
-#             actor.height = int(each['value'])
-#
-#         if int(each['value']) <=154:
-#             actor.category ="1"
-#         elif 155 <=int(each['value']) <=159:
-#             actor.category="2"
-#         elif 160 <=int(each['value']) <=164:
-#             actor.category="3"
-#         elif int(each['value']) >=165:
-#             actor.category="4"
-#         else:
-#             actor.category="2"
-#         flash(u"저장 완료")
-#         db.session.commit()
-#
-#     return render_template("crawl.html", final=final)
+@app.route('/crawling/<int:page>',defaults={'page':1})
+@app.route('/crawling/<int:page>',methods=['GET', 'POST'])
+def crawling(page):
+    # 배우랑 키 가져오기
+    a = Actor.query.order_by(desc(Actor.average)).offset((page - 1) * 20).limit(20)
+    actor = []
+    for i in a:
+        actor.append(i.name.replace(" ","") )
+
+    final=[]
+    for each in actor:
+
+        url = "http://hentaku.tistory.com/entry/" + str(each)
+        try:
+            source = urllib2.urlopen( url ).read()
+        except:
+            continue
+        soup = BeautifulSoup( source ,from_encoding="utf-8")
+
+        list=""
+        for total in soup.find_all("div","avstar_info_b"):
+            list=total.text
+        # logging.error(list)
+
+        # final = []
+        name = re.compile(u"[^ \u3131-\u3163\uac00-\ud7a3]+")
+        height = re.compile("[1][4-8][0-9]")
+
+        result1 = name.sub("",list)
+        result2 = height.findall(list)
+        str2 = ''.join(result2)
+
+        final.append(dict(name=result1,value=str2))
+
+
+        logging.error(final)
+        # logging.error(result1)
+        # logging.error(result2)
+
+
+    for each in final:
+        actor = Actor.query.get(each['name'])
+        if actor == None:
+            continue
+
+        if each['value'] =="" :
+            actor.height = 155
+            each['value'] = 155
+        else:
+            actor.height = int(each['value'])
+
+        if int(each['value']) <=154:
+            actor.category ="1"
+        elif 155 <=int(each['value']) <=159:
+            actor.category="2"
+        elif 160 <=int(each['value']) <=164:
+            actor.category="3"
+        elif int(each['value']) >=165:
+            actor.category="4"
+        else:
+            actor.category="2"
+        flash(u"저장 완료")
+        db.session.commit()
+
+    return render_template("crawl.html", final=final)
